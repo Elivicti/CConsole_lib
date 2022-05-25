460843f1304ca2f0522f217b48217bbf82bc6cb3
Edwyn 向 <edwynb72@gmail.com>
2022-05-25 10:05:47 +0800

    test

--- test/test.c
+++ test/test.c
@@ -2,12 +2,20 @@
 
 #define __CCONSOLE_TEST___
 #include "../CConsole.h"
-#include <conio.h>
-
-#include <stdarg.h>
 
 int main()
 {
+	initConsole();
+	TextUi* ui = readTextUi("testui.txt");
+	CursorAnchor anchor = createCursorAnchor(0, 0);
+	setTextAttribute(Red, White);
+	drawTextUi(ui, &anchor);
+	resetTextAttribute(All);
+	int ch = getPressedKey();
+	printf("\n%c pressed.\n", ch);
+	
+	deleteTextUi(ui);
+	ui = NULL;
 	
 	return 0;
 }
