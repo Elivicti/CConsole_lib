91eff9a4ed3ac392e002774a0230131af4dd8fcc
Edwyn 向 <edwynb72@gmail.com>
2022-05-29 20:58:59 +0800

    Added a macro

--- CConsole.h
+++ CConsole.h
@@ -5,6 +5,12 @@
 #pragma comment(lib,"CConsole.lib")
 #endif
 
+#ifdef __cplusplus
+#define NULL_PTR nullptr
+#else
+#define NULL_PTR NULL
+#endif
+
 #include "CConsole_lib.h"
 
 #endif //! __C_CONCOLE_HEADER___
