d74c78ab08b31e726e6972cdb972e8ae0a797947
Edwyn 向 <edwynb72@gmail.com>
2022-06-04 15:16:38 +0800

    Updated

--- build.bat
+++ build.bat
@@ -13,7 +13,7 @@ ar -cr .\build\CConsole.lib CConsole_lib.o
 rm CConsole_lib.o
 
 if "%1" == "test" (
-	gcc -std=c11 .\test\test.c -l:CConsole.lib -L.\build -o .\test\test.exe
+	gcc -std=c11 .\test\test.c -l:CConsole.lib -L.\build -lm -o .\test\test.exe
 	if "%2" == "-r" (
 		cd .\test\
 		.\test.exe
